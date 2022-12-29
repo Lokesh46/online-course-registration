795ac60a905777d990c7daef3830fe7dc0677d26
Lokesh46 <78641702+Lokesh46@users.noreply.github.com>
2022-12-29 19:14:43 +0530

    Update db.php

--- project/admin/db.php
+++ project/admin/db.php
@@ -1,9 +1,9 @@
 <?php
-$connection = mysqli_connect('localhost','id20030913_lokeshadmin', 'L%jRteKEj5u^kp1%','id20030913_course_reg');
+$connection = mysqli_connect('localhost','root', '','course_reg');
 if (!$connection){
     die("Database Connection Faileddd" . mysqli_error($connection));
 }
-$select_db = mysqli_select_db($connection,'id20030913_course_reg');
+$select_db = mysqli_select_db($connection,'course_reg');
 if (!$select_db){
     die("Database Selection Failed" . mysqli_error($connection));
 }
